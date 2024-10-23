b539af8ae8bf9a35fbf071e9f53afa9b172287ea
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2024-10-23 14:31:51 +0300

    minor changes

--- sportify/pkg/middleware/config.go
+++ sportify/pkg/middleware/config.go
@@ -13,15 +13,17 @@ const (
 	ConfigKey ctxKeyConfig = 0
 )
 
+// Config is a middleware that adds the pointer to the config to the context.
+// Any configuration values must be taken from the context and not from the global config.
 func Config(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
+	inner := func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		cfg := config.GetGlobalConfig()
 		if cfg == nil {
-			panic("config is nil") // TODO: handle this error !!!
+			panic("config is nil") // FIXME: handle this error !!!
 		}
 		ctx = context.WithValue(ctx, ConfigKey, ctx)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	}
-	return http.HandlerFunc(fn)
+	return http.HandlerFunc(inner)
 }
