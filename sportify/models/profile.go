7965aabd1ffdb455063c1ecb66e515e552b61899
vilinvil <ivn15072002@gmail.com>
2024-12-04 09:27:59 +0300

    47 move back api representation of get profile

--- sportify/models/profile.go
+++ sportify/models/profile.go
@@ -8,21 +8,17 @@ import (
 	"github.com/google/uuid"
 )
 
-type ProfileInfo struct {
-	UserID      string      `json:"user_id"`
-	Username    string      `json:"username"`
-	FirstName   *string     `json:"first_name"`
-	SecondName  *string     `json:"second_name"`
-	PhotoURL    *string     `json:"photo_url"`
-	Description *string     `json:"description"`
-	TgURL       *string     `json:"tg_url"`
-	SportTypes  []SportType `json:"sport_types"`
-}
-
 type ProfileAPI struct {
 	IsMyProfile  bool        `json:"isMyProfile"`
 	IsTgOnlyUser bool        `json:"isTgOnlyUser"`
-	ProfileInfo  ProfileInfo `json:"profile_info"`
+	UserID       string      `json:"user_id"`
+	Username     string      `json:"username"`
+	FirstName    *string     `json:"first_name"`
+	SecondName   *string     `json:"second_name"`
+	PhotoURL     *string     `json:"photo_url"`
+	Description  *string     `json:"description"`
+	TgURL        *string     `json:"tg_url"`
+	SportTypes   []SportType `json:"sport_types"`
 }
 
 func mapTgURL(tgUserID *int64, tgUsername string) *string {
@@ -37,16 +33,14 @@ func MapUserFullToProfileAPI(userIDFromToken uuid.UUID, userFull *UserFull) *Pro
 	return &ProfileAPI{
 		IsMyProfile:  userIDFromToken == userFull.ID,
 		IsTgOnlyUser: userFull.TgID != nil,
-		ProfileInfo: ProfileInfo{
-			UserID:      userFull.ID.String(),
-			Username:    userFull.Username,
-			FirstName:   userFull.FirstName,
-			SecondName:  userFull.SecondName,
-			PhotoURL:    userFull.PhotoURL,
-			Description: userFull.Description,
-			TgURL:       mapTgURL(userFull.TgID, userFull.Username),
-			SportTypes:  userFull.SportTypes,
-		},
+		UserID:       userFull.ID.String(),
+		Username:     userFull.Username,
+		FirstName:    userFull.FirstName,
+		SecondName:   userFull.SecondName,
+		PhotoURL:     userFull.PhotoURL,
+		Description:  userFull.Description,
+		TgURL:        mapTgURL(userFull.TgID, userFull.Username),
+		SportTypes:   userFull.SportTypes,
 	}
 }
 
